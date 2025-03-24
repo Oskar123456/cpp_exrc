f8781328267a5f89f8899f7a3e3dbef252919aca
oskar123456 <cph-oh82@cphbusiness.dk>
2025-03-24 13:08:34 +0100

    u

--- src/main.cpp
+++ src/main.cpp
@@ -17,8 +17,48 @@ License:            none
 
 using namespace std;
 
+int log2_floor(u64 n)
+{
+    int log = -1;
+    while (n > 0) {
+        ++log;
+        n /= 2;
+    }
+    return log;
+}
+
+string u64_print_bin(u64 n, int leading_zeroes)
+{
+    string bin_str;
+    for (int i = 0; n > 0; n /= 2) {
+        bin_str = (char)(((n >> i) & 1) + '0') + bin_str;
+    }
+    while (leading_zeroes-- > 0) {
+        bin_str = '0' + bin_str;
+    }
+    return bin_str;
+}
+
+u64 u64_append_one(u64 n)
+{
+    return n + (1 << (log2_floor(n) + 1));
+}
+
+u64 mystery(u64 n)
+{
+    while (n > 0) {
+        cout << n << " : " << u64_print_bin(n, 2) << endl;
+        n /= 2;
+    }
+    return n;
+}
+
 int main(int argc, char *argv[])
 {
+
+    for (int i = 1; i < 101; ++i) {
+        mystery(i);
+    }
 
     return 0;
 }
